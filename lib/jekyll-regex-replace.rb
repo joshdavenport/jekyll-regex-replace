3ef67ef2f51762626e395b0c87f80cf556215fca
Josh Davenport <josh@joshdavenport.co.uk>
2018-11-12 18:33:35 +0000

    Added regex_replace_once filter

--- lib/jekyll-regex-replace.rb
+++ lib/jekyll-regex-replace.rb
@@ -6,6 +6,11 @@ module Jekyll
       regex = /#{regex_search}/
       return str.gsub(regex, value_replace)
     end
+
+    def regex_replace_once(str, regex_search, value_replace)
+      regex = /#{regex_search}/
+      return str.sub(regex, value_replace)
+    end
   end
 end
 
