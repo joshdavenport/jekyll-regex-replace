291e1b5bdc112aaceaba8830ee83effbcc385190
Vishvas Vasuki विश्वासः <vishvas.vasuki@gmail.com>
2018-11-11 21:23:52 -0800

    Update jekyll-regex-replace.rb
    Users will typically want ALL occurrences replaced.
    Reference - https://ruby-doc.org/core-2.2.0/String.html

--- lib/jekyll-regex-replace.rb
+++ lib/jekyll-regex-replace.rb
@@ -4,7 +4,7 @@ module Jekyll
   module RegexReplace
     def regex_replace(str, regex_search, value_replace)
       regex = /#{regex_search}/
-      return str.sub(regex, value_replace)
+      return str.gsub(regex, value_replace)
     end
   end
 end
